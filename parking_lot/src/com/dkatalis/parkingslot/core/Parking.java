0c9cebd8246ad037141836796ac3d423d734dad5
KonzernTechie <kv.nijesh@gmail.com>
2020-07-12 14:00:15 +0530

    test park

--- parking_lot/src/com/dkatalis/parkingslot/core/Parking.java
+++ parking_lot/src/com/dkatalis/parkingslot/core/Parking.java
@@ -12,53 +12,70 @@ import com.dkatalis.parkingslot.model.Ticket;
  *
  */
 public class Parking<E> extends ParkingLot<E> {
-	
 
 	public Parking(int capacity) {
 		super(capacity);
 		System.out.println("Created Parking lot with " + capacity + " slots");
 	}
-	
+
 	public void park(Ticket ticket) {
 		try {
 			int position = findNearestAvailableSlot();
 			insert(position, (E) ticket);
 			ticket.setSlot(getSlot(position));
-			System.out.println("Allocated slot number :"+ticket.getSlot());
+			System.out.println("Allocated slot number :" + ticket.getSlot());
 		} catch (IllegalAccessException e) {
 			e.printStackTrace();
 		}
+
+	}
+
+	
+	public void remove(Ticket ticket) {
+		try {
+
+			int position = getPostion(ticket.getSlot());
+			delete(position);
+			calculateParkingCharge(ticket);
+			System.out.println(" Registration number " + ticket.getRegistrationNumber() + " with Slot Number "
+					+ ticket.getSlot() + " is free with  Charge " + ticket.getCharge());
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	public void status() {
 		
 	}
 	
 	private int getSlot(int position) {
-		return position +1;
+		return position + 1;
 	}
-	
+
 	private int getPostion(int slot) {
-		return slot -1;
+		return slot - 1;
 	}
 
-	public void remove(Ticket ticket) {
-		try {
-			
-			int position = getPostion(ticket.getSlot());
-			delete(position);
-			
-			int charge =0;
-			System.out.println(" Registration number "+ticket.getRegistrationNumber()+" with Slot Number "+ticket.getSlot() +" is free with  Charge "+charge );
-			
-		}catch (Exception e) {
-			e.printStackTrace();
+
+
+	private void calculateParkingCharge(Ticket ticket) {
+		int hour = ticket.getHour();
+		double charge = 0;
+
+		if (hour <= 0) {
+			charge = 0;
+		} else if (hour < 2) {
+			charge = 10;
+		} else {
+			charge = ((hour - 2) * 10) + 10;
 		}
+		ticket.setCharge(charge);
 	}
-
-
 
 	private int findNearestAvailableSlot() throws IllegalAccessException {
 		return firstUnallocatedSpace();
 	}
-	
-	
 
 }
